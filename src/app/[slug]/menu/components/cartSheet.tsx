6f81a8eda63c4cb6182b2414dee591488283940a
Lucas Zioli <lucas.zioli.04@gmail.com>
2025-02-23 23:14:50 -0300

    feat: Adiciona componente de finalização de pedido

--- src/app/[slug]/menu/components/cartSheet.tsx
+++ src/app/[slug]/menu/components/cartSheet.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
@@ -12,8 +12,10 @@ import { formatCurrency } from "@/helpers/formatCurrency";
 
 import { CartContext } from "../context/cart";
 import CartProduct from "./cartProduct";
+import FinishOrderDialog from "./finish-order-dialog";
 
 const CartSheet = () => {
+  const [finishOrderIsOpen, setFinishOrderIsOpen] = useState(false);
   const { isOpen, toggleCart, products, total } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
@@ -35,7 +37,16 @@ const CartSheet = () => {
               </div>
             </CardContent>
           </Card>
-          <Button className="w-full rounded-full">Finalizar Pedido</Button>
+          <Button
+            onClick={() => setFinishOrderIsOpen(true)}
+            className="w-full rounded-full"
+          >
+            Finalizar Pedido
+          </Button>
+          <FinishOrderDialog
+            open={finishOrderIsOpen}
+            onOpenChange={setFinishOrderIsOpen}
+          ></FinishOrderDialog>
         </div>
       </SheetContent>
     </Sheet>
---
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -0,0 +1,120 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "Nome é obrigatório",
+  }),
+  email: z
+    .string()
+    .trim()
+    .min(1, {
+      message: "Email é obrigatório",
+    })
+    .email("Esse email não é valido"),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface FinishOrderDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+    shouldUnregister: true,
+  });
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  };
+  return (
+    <Drawer open={open} onOpenChange={onOpenChange}>
+      <DrawerTrigger asChild></DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Finalizar Pedido</DrawerTitle>
+          <DrawerDescription>
+            Insira suas informações abaixo para finalizar o pedido
+          </DrawerDescription>
+        </DrawerHeader>
+        <div className="p-5">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu nome..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu email..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DrawerFooter>
+                <Button
+                  type="submit"
+                  variant="destructive"
+                  className="rounded-full"
+                >
+                  Finalizar
+                </Button>
+                <DrawerClose asChild>
+                  <Button className="w-full rounded-full" variant="outline">
+                    Cancelar
+                  </Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </form>
+          </Form>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default FinishOrderDialog;
